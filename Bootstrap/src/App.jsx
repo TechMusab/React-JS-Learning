f120d4941655e1a87a20b8b0a46791e7e741af89
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-09 15:01:01 -0700

    some bootstrap

---
+++ Bootstrap/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+
+function App() {
+  return <div>
+  <button type="button" className="btn btn-primary">Primary</button>
+</div>;
+}
+// class is a reserved keyword in js so use className
+export default App;
