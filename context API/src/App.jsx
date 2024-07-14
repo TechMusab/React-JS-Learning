9c1f1bee4969bbc7db7092f0cbca1cddca7a5830
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-13 19:14:56 -0700

    seprated the ui and logic

--- context API/src/App.jsx
+++ context API/src/App.jsx
@@ -2,58 +2,29 @@ import "./App.css";
 import AppName from "./components/AppName";
 import Addtodo from "./components/Addtodo";
 import Todocontainer from "./components/todoitemcontainer";
-import { useReducer } from "react";
-import { Todoitemcontext } from "./store/todoitemcontext";
+import Todoitemprovider from "./store/todoitemcontext";
 //when you dont want ui repainting use useref hook
 //used directly in jsx components
 //understand break the app into components
-const todoitemsreducer = (state, action) => {
-  let newstate = state;
-  if (action.type === "ADD") {
-    newstate = [
-      ...state,
-      { name: action.payload.name, date: action.payload.date },
-    ];
-  } else if (action.type === "DELETE") {
-    newstate = state.filter((item) => item.name !== action.payload.name);
-  }
-  return newstate;
-};
+/*
+named import use braces
+default import not use braces
+ */
 
 function App() {
-  // let [todoitem, settodoitem] = useState([]);
-  let [todoitem, dispatchtodoitem] = useReducer(todoitemsreducer, []);
-  const addtodoitem = (name, date) => {
-    const newaction = {
-      type: "ADD",
-      payload: {
-        name: name,
-        date: date,
-      },
-    };
-    dispatchtodoitem(newaction);
-    //functionl update if my current value depends upon previous value
-  };
-  const deletetodo = (name) => {
-    const newaction = {
-      type: "DELETE",
-      payload: {
-        name: name,
-      },
-    };
-    dispatchtodoitem(newaction);
-  };
-  return (
-    <Todoitemcontext.Provider
-      value={{ todoitem: todoitem, add: addtodoitem, deletetodo: deletetodo }}
-    >
+return (
+    <Todoitemprovider>
       <center>
         <AppName></AppName>
         <Addtodo></Addtodo>
         <Todocontainer></Todocontainer>
       </center>
-    </Todoitemcontext.Provider>
+    </Todoitemprovider>
   );
 }
+//understanding
+/*
+when todoitemprovider renders and replaces it self with return statement of it then when add button is clicked dd ftn calls then dispatcher of reducer hook updates the state and then whole app rerenders
+ */
 export default App;
 //disadvantage of contxt hook is that then whole ui repaints
--- context API/src/store/todoitemcontext.jsx
+++ context API/src/store/todoitemcontext.jsx
@@ -1,3 +1,48 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
  export const Todoitemcontext=createContext();
+ const Todoitemprovider=({children})=>{
+    const todoitemsreducer = (state, action) => {
+        let newstate = state;
+        if (action.type === "ADD") {
+          newstate = [
+            ...state,
+            { name: action.payload.name, date: action.payload.date },
+          ];
+        } else if (action.type === "DELETE") {
+          newstate = state.filter((item) => item.name !== action.payload.name);
+        }
+        return newstate;
+      };
+      // let [todoitem, settodoitem] = useState([]);
+  let [todoitem, dispatchtodoitem] = useReducer(todoitemsreducer, []);
+  const addtodoitem = (name, date) => {
+    const newaction = {
+      type: "ADD",
+      payload: {
+        name: name,
+        date: date,
+      },
+    };
+    dispatchtodoitem(newaction);
+    //functionl update if my current value depends upon previous value
+  };
+  const deletetodo = (name) => {
+    const newaction = {
+      type: "DELETE",
+      payload: {
+        name: name,
+      },
+    };
+    dispatchtodoitem(newaction);
+  };
+  return (
+    <Todoitemcontext.Provider
+      value={{ todoitem, add: addtodoitem, deletetodo }}
+    >
+      {children}
+    </Todoitemcontext.Provider>
+  );
+  
+ }
+ export default Todoitemprovider;
