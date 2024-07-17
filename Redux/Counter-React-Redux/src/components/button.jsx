5d9b3486ea5457ced2e49dc27db120f1dc0c7b5f
Musab Bin Ubaid <joiya4636@gmail.com>
2024-07-17 15:01:10 -0700

    created the small counter project using react-redux

--- Redux/Counter-React-Redux/src/components/button.jsx
+++ Redux/Counter-React-Redux/src/components/button.jsx
@@ -1,28 +1,56 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Button = () => {
-
-const dispatch=useDispatch();
-const Increase=()=>{
-dispatch({
-  type: "increase",
-
-})
-}
-const decrease=()=>{
-  dispatch({
-    type: "decrease",
-  })
-}
+  const dispatch = useDispatch();
+  const input=useRef();
+  const Increase = () => {
+    dispatch({
+      type: "increase",
+    });
+  };
+  const decrease = () => {
+    dispatch({
+      type: "decrease",
+    });
+  };
+  const handleadd = () => {
+    dispatch({
+      type: "add",
+      payload:{
+        num:input.current.value,
+      }
+    });
+    input.current.value =" ";
+  };
+  const handlesubtract = () => {
+    dispatch({
+      type: "subtract",
+      payload:{
+        num:input.current.value,
+      }
+    });
+    input.current.value =" ";
+  };
+  const handleprivacy=()=>{
+    dispatch({
+      type:"privacy",
+    });
+  }
   return (
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
       <button type="button" className="btn btn-primary" onClick={Increase}>
-       Increase Counter
+       +1
       </button>
       <button type="button" className="btn btn-success" onClick={decrease}>
-       Decrease Counter
+      -1
       </button>
-
+      <button type="button" className="btn btn-warning" onClick={handleprivacy}>
+       Privacy Toggle
+      </button>
+      <input ref={input} type="text" placeholder="Enter Number" />
+      <button className="btn btn-info" onClick={handleadd}>Add</button>
+      <button className="btn btn-danger" onClick={handlesubtract}>Subtract</button>
     </div>
   );
 };
--- Redux/Counter-React-Redux/src/store/index.js
+++ Redux/Counter-React-Redux/src/store/index.js
@@ -1,22 +1,37 @@
 import { createStore } from "redux";
 const Intial = {
   count: 0,
+  privacy: false,
 };
 function reducer(store = Intial, action) {
   if (action.type == "increase") {
     return {
-        //New state object
+      //New state object
+      ...store,
       count: store.count + 1,
+      //use spread operator to copy the old state and update the new state
     };
   } else if (action.type == "decrease") {
-    {
-      return {
-        count: store.count - 1,
-      };
-    }
+    return {
+      ...store,
+      count: store.count - 1,
+    };
+  } else if (action.type == "add") {
+    return {
+      ...store,
+      count: store.count + Number(action.payload.num),
+    };
+  } else if (action.type == "subtract") {
+    return {
+      ...store,
+      count: store.count - Number(action.payload.num),
+    };
+  } else if (action.type == "privacy") {
+    return { ...store, privacy: !store.privacy };
   }
-  console.log("Invalid action");
+
   return store;
 }
 export const store = createStore(reducer);
 //subscrition is already setup using useSelector in react
+//problem face when i am changing value the other value not passed so unexpected results happen so you pass all the other values 
